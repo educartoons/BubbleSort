e8ea675677df562b83441679def4f6465c5902b3
Eduar Apaza Yáñez <educartoons@gmail.com>
2016-08-12 21:17:04 -0500

    Bubble Sort

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -7,9 +7,49 @@
 //
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void bubbleSort(int *, const int);
+void swap(int * const, int * const);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    const int sizeArray = 10;
+    
+    int a[sizeArray] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+    
+    cout<<"Elements in the array: "<<endl;
+    
+    for(int i=0; i<sizeArray; i++){
+        cout<< setw(4)<<a[i];
+    }
+    cout<<endl;
+    
+    bubbleSort(a, sizeArray);
+    
+    cout<<"Elements sorted in the array: "<<endl;
+    
+    for(int i=0; i<sizeArray; i++){
+        cout<< setw(4)<<a[i];
+    }
+    
     return 0;
+}
+
+void bubbleSort(int *a, const int size){
+    for(int i=0; i<size-1;i++){
+        for(int j=0; j<size-1;j++){
+            if(a[j]>a[j+1]){
+                swap(&a[j], &a[j+1]);
+            }
+        }
+    }
+}
+
+void swap(int * const a, int * const b){
+    int aux = *a;
+    *a = *b;
+    *b = aux;
 }
